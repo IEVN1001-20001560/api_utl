ab35edbb25966ba5d0384b15471781ed055b1ea6
Ceidy Martinez <ceidy117@gmail.com>
2024-10-29 20:55:28 -0600

    Tercer commit INSERT 29 de octubre

--- src/app.py
+++ src/app.py
@@ -29,28 +29,26 @@ def lista_alumnos():
     
     
     
- 
- def leer_alumno_bd(matricula):
-     try: 
+def leer_alumno_bd(matricula):
+    try: 
         cursor=con.connection.cursor()
         sql="select * from alumnos where matricula={}".format(matricula)
         cursor.execute(sql)
-        datos=cursor.fetchall()
-        alumnos=[]
+        datos=cursor.fetchone()
         if datos!=None:
              
             alumno={"matricula": datos[0],"nombre":datos[1],
                     "apaterno":datos[2],"amaterno":datos[3],
                     "correo":datos[4]}
-        return alumno
-    else:
-        return None
+            return alumno
+        else:            
+            return None
     
     except Exception as ex:
         return jsonify({"message": "error{}".format(ex),'exito':False})
     
         
-@app.route("/alumnos/<math>", methods=['GET'])
+@app.route("/alumnos/<mat>", methods=['GET'])
 def leer_alumno(mat):
     try: 
         alumno=leer_alumno_bd(mat)
@@ -62,6 +60,26 @@ def leer_alumno(mat):
     except Exception as ex:
         return jsonify({"message": "error{}".format(ex),'exito':False})
 
+@app.route("/alumnos", methods=['POST'])
+def registrar_alumno():
+    try:
+        alumno=leer_alumno_bd(request.json['matricula'])
+        if alumno!=None:
+            return jsonify({'mensaje':"Alumno ya existente, no se uede dulicar", 'exito':False})
+        else:
+            cursor=con.connection.cursor()
+            sql="insert into alumnos (matricula,nombre,apaterno,amaterno,correo) values ('{0}','{1}','{2}','{3}','{4}')".format(request.json['matricula'],
+                                                                                                                      request.json['nombre'],
+                                                                                                                      request.json['apaterno'],
+                                                                                                                      request.json['amaterno'],
+                                                                                                                      request.json['correo'],)
+        cursor.excecute(sql)
+        con.connection.commit()
+        return jsonify({'mensaje':"Alumno registrado", "exito":True})
+               
+    except Exception as ex:
+        pass    
+   
 def pagina_no_encontrada(error):
     return "<h1>Pagina no encontrada</h1>"
 if __name__=="__main__":
